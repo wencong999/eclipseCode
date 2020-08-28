5d7c79ab37a0d85c50580893045149255090ac91
lll <909520@163.com>
2020-08-28 11:04:00 +0800

    第一次上传

---
+++ src/com/kgc/domain/User.java
@@ -0,0 +1,17 @@
+package com.kgc.domain;
+
+import java.util.Scanner;
+
+public class User  {
+	public static void main(String[] args) {
+		System.out.println("¹þ¹þ");
+		Scanner sc = new Scanner(System.in);
+		method(10);
+	}
+	
+	public static void method(int num) {
+		for (int i = 0; i < num; i++) {
+			System.out.println(i);
+		}
+	}
+}
